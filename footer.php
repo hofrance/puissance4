9dda4f7c20b62abbca21c4de4dadeab2f50a1bb1
hofrance <hofrancebankouezi@gmail.com>
2024-01-29 00:04:48 +0100

    Premier commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!-- footer.php -->
+
+<div class="footer-basic">
+    <footer>
+        <!-- Section des médias sociaux -->
+        <div class="social-icons">
+            <a href="#" class="icon-link"><i class="icon ion-social-instagram"></i></a>
+            <a href="#" class="icon-link"><i class="icon ion-social-snapchat"></i></a>
+            <a href="#" class="icon-link"><i class="icon ion-social-twitter"></i></a>
+            <a href="#" class="icon-link"><i class="icon ion-social-facebook"></i></a>
+        </div>
+
+        <!-- Section des liens -->
+        <div class="footer-links">
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="./presentation_du_jeu.html">Présentation du Jeu</a></li>
+                <li class="list-inline-item"><a href="./algorithme_du_jeu.html">Algorithme du Jeu</a></li>
+                <li class="list-inline-item"><a href="./presentation_ia.html">IA du Jeu</a></li>
+                <li class="list-inline-item"><a href="./presentation_groupe.html">Notre Équipe</a></li>
+            </ul>
+        </div>
+
+        <!-- Section des informations de contact -->
+        <div class="footer-contacts">
+            <p class="contact-item"><a href="mailto:mussabarry.mb@icloud.com"> BARRY Moussa</a></p>
+            <p class="contact-item"><a href="mailto:richardhofrance.bankouezi@etud.univ-angers.fr">Richard BANKOUEZI</a></p>
+        </div>
+
+        <!-- Droits d'auteur -->
+        <p class="copyright">© 2022 Université d'Angers. Tous droits réservés.</p>
+    </footer>
+</div>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- header.php -->
+
+<header>
+    <nav>
+        <ul class="menu">
+            <li><a href="./presentation_du_jeu.html">Présentation du jeu</a></li>
+            <li><a href="./algorithme_du_jeu.html">Algorithme du jeu</a></li>
+            <li><a href="./presentation_ia.html">L'intelligence du jeu</a></li>
+            <li><a href="./presentation_groupe.html">Découvrez notre équipe</a></li>
+        </ul>
+    </nav>
+</header>
